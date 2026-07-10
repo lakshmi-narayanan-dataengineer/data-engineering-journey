fa1f3cb5e7921aa1ad72b5e4322d5ba444f2f630
lakshmi-narayanan-dataengineer <vlakshnarayanan06@gmail.com>
2026-07-10 18:56:59 +0530

    With Outputs

--- Day_3/conditionals.py
+++ Day_3/conditionals.py
@@ -7,6 +7,7 @@ age = int(input("Enter your age: "))
 if age <= 5:
     print("You are a Toddler")
 #if → executes code if condition is true  
+#Output Enter your age: 5 , You are a toddler
 
 #Example 2: if-else (Voting Eligibility)
 #Real-world use case: Government systems checking if a citizen meets the minimum voting age.
@@ -16,6 +17,20 @@ if age >= 18:
     print("You are eligible to vote in the election")
 else:
     print("You are not eligible to vote")
+
+"""Output
+Case 1: Age ≥ 18
+
+Enter your age: 20
+Condition age >= 18 is True, so the output is:
+You are eligible to vote in the election 
+
+Case 2: Age < 18
+Enter your age: 15
+Condition age >= 18 is False, so the else block runs:
+
+You are not eligible to vote
+"""
 
 #Example 3: if-elif-else (Traffic Signal)
 #Real-world use case: A driving simulator applying traffic rules based on signal lights.
@@ -28,6 +43,16 @@ elif signal.lower() == "yellow":
 else:
     print("Go")
 
+"""Output
+Input: red
+Stop your vehicle for 70 seconds
+
+Input: yellow
+Get ready to go
+
+Input: green (or any other color)
+Go
+"""
 #Example 4: Nested if (License Checker)
 #Real-world use case: Transport department system validating both age and license before issuing a driving permit.
 
@@ -41,3 +66,19 @@ if age >= 18:
         print("Please take a license to drive")
 else:
     print("You are not eligible to drive")
+
+"""
+Output
+Case 1
+Age ≥ 18 and License = Yes
+You are eligible to drive
+
+Case 2
+Age ≥ 18 and License = No
+Please take a license to drive
+
+Case 3
+Age < 18 (any license input)
+You are not eligible to drive
+"""
+

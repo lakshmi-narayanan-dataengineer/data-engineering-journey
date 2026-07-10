e8a4d28e931c36da05d5b8339f712319944dfdaf
lakshmi-narayanan-dataengineer <vlakshnarayanan06@gmail.com>
2026-07-10 10:27:21 +0530

    Variables and Data Types

--- Day-1.py
+++
@@ -1,7 +0,0 @@
-print("I am Lakshmi Narayanan . I am Starting Today ")
-name = "Lakshmi Narayanan"
-goal = "Data Engineer"
-months_left = 6
-print(f"My name is {name}")
-print(f"Goal is {goal}")
-print(f"I have {months_left} months to make it happen")
---
+++ Day_1/variables.py
@@ -0,0 +1,12 @@
+"""
+Variables are used to store values in memory for later use.
+Without variables, you would need to manually change the value
+everywhere it appears in your code.
+"""
+
+# Without using a variable
+print(10)  # You must overwrite this value everywhere if it changes
+
+# Using a variable
+x = 10
+print(x)  # Easier to update: just change the value of 'x'
